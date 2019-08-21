4f369d9b1fbc0ceeff984fcd34607be440392664
fijisoo <benjamindomninik@gmail.com>
2019-08-21 20:27:54 +0200

    init with default sort (without lodash)

---
+++ src/app/app.component.ts
@@ -0,0 +1,156 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'tomtom';
+  tab = [
+    {
+      id: '235-231',
+      name: 'Basia',
+      level: 0,
+      children: [{
+        id: '534-23w',
+        name: 'Wacek',
+        level: 1,
+        children: [{
+          id: '134-23w',
+          name: 'Zmalal',
+          level: 2,
+          children: []
+        },
+          {
+            id: '3gg-222',
+            name: 'Urus',
+            level: 2,
+            children: []
+          }
+        ]
+      },
+        {
+          id: 'hgg-222',
+          name: 'Placek',
+          level: 1,
+          children: []
+        },
+        {
+          id: '111-w',
+          name: 'Zadek',
+          level: 1,
+          children: [{
+            id: 'www-23w',
+            name: 'Milenka',
+            level: 2,
+            children: []
+          },
+            {
+              id: 'aaa-222',
+              name: 'Ilona',
+              level: 2,
+              children: []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'w30-444',
+      name: 'Andrzej',
+      level: 0,
+      children: [{
+        id: '534-23w',
+        name: 'Wacek',
+        children: [{
+          id: '134-23w',
+          name: 'Urus',
+          children: []
+        },
+          {
+            id: '3gg-222',
+            name: 'Zmalal',
+            children: []
+          }
+        ]
+      }]
+    },
+    {
+      id: '986-w',
+      name: 'Ala',
+      level: 0,
+      children: [{
+        id: '534-23w',
+        name: 'Wacek',
+        level: 1,
+        children: [{
+          id: '134-23w',
+          name: 'Zmalal',
+          level: 2,
+          children: []
+        },
+          {
+            id: '3gg-222',
+            name: 'Urus',
+            level: 2,
+            children: []
+          }
+        ]
+      },
+        {
+          id: '112-333',
+          name: 'Kacek',
+          level: 1,
+          children: [{
+            id: '134-23w',
+            name: '2222',
+            level: 2,
+            children: []
+          },
+            {
+              id: '3gg-222',
+              name: '1111',
+              level: 2,
+              children: []
+            }
+          ]
+        }
+      ]
+    }
+  ];
+
+  ngOnInit() {
+    const newTab = this.sortTableAlphabetically(this.tab, (x) => x.name, (x) => x.children);
+    console.log(newTab);
+  }
+
+  sortTableAlphabetically (tab: any, sortBy: any, nestedNode: any) {
+    if (Array.isArray(tab) && tab.length) {
+      tab.sort(this.compareValues(sortBy));
+      // tab = orderBy(tab, sortBy);
+      tab.map(el => {
+        if ((Array.isArray(nestedNode(el))) && nestedNode(el).length) {
+          this.sortTableAlphabetically(nestedNode(el), sortBy, nestedNode);
+        }
+      });
+      return tab;
+    }
+    return tab;
+  }
+
+  compareValues(value: any) {
+    return function(a: any, b: any): number {
+      function getValue (val) {
+        return value(val).toString().toUpperCase();
+      }
+      return getValue(a).localeCompare(getValue(b));
+    };
+  }
+
+
+
+// użycie:
+  /* sortTableAlphabetically<Group>(groups, (x: any) => x.name, (x: any) => x.children); */
+
+}
